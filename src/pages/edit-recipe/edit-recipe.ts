79ada5aaa74154e059b185b5492bdf2c02ee7c90
nuneza <arnulfo.nunez@stifel.com>
2017-03-03 14:42:13 -0500

    training

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms/src/directives';
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
@@ -8,7 +9,10 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class EditRecipePage implements OnInit{
 
+//@ViewChild('theForm') theForm: NgForm;
+ //protected theForm: NgForm = null; 
  protected mode: string = 'New';
+ protected diffiCultyLevelOptions: string[] = ['Easy','Medium','Hard'];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
@@ -19,4 +23,11 @@ export class EditRecipePage implements OnInit{
 ngOnInit(){
   this.mode = this.navParams.get('mode');
 }
+
+protected onSubmitRecipe(theForm: NgForm): void{
+//console.log(theForm);
+//console.log(this.theForm);
+theForm.value.title = "a test";
+}
+
 }
